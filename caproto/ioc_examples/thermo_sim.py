0bfd6c77818672f788006c1406fc3e7981d36d03
Thomas A Caswell <tcaswell@bnl.gov>
2018-11-18 14:35:05 +1100

    ENH: add a 'state' PV to the thermo simulator
    This lets the IOC report when it is done.

--- caproto/ioc_examples/thermo_sim.py
+++ caproto/ioc_examples/thermo_sim.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from caproto.server import pvproperty, PVGroup, ioc_arg_parser, run
+from caproto import ChannelType
 import numpy as np
 import time
 from textwrap import dedent
@@ -30,15 +31,23 @@ class Thermo(PVGroup):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._T0 = time.monotonic()
+        self._entered_band = -1
 
     readback = pvproperty(value=0, dtype=float, read_only=True,
                           name='I',
-                          mock_record='ai')
+                          mock_record='ai', units='K')
 
-    setpoint = pvproperty(value=100, dtype=float, name='SP')
-    K = pvproperty(value=10, dtype=float)
-    omega = pvproperty(value=np.pi, dtype=float)
-    Tvar = pvproperty(value=10, dtype=float)
+    setpoint = pvproperty(value=100, dtype=float, name='SP',
+                          mock_record='ai', units='K')
+    K = pvproperty(value=10, dtype=float, units='1/K')
+    omega = pvproperty(value=np.pi, dtype=float, units='Hz')
+    Tvar = pvproperty(value=10, dtype=float, units='K')
+
+    state = pvproperty(value=0, dtype=ChannelType.ENUM,
+                       enum_strings=['stable', 'settling'],
+                       read_only=True)
+    deadband = pvproperty(value=2, dtype=float)
+    settle_time = pvproperty(value=2, dtype=float, units='s')
 
     @readback.scan(period=.1, use_scan_field=True)
     async def readback(self, instance, async_lib):
@@ -54,11 +63,28 @@ class Thermo(PVGroup):
                   **{k: getattr(self, k).value
                      for k in ['setpoint', 'K', 'omega', 'Tvar']})
 
+        setpoint = self.setpoint.value
+        if np.abs(setpoint - T) > self.deadband.value:
+            self._entered_band = -1
+        elif self._entered_band < 0:
+            self._entered_band = time.monotonic()
+
+        if self._entered_band > 0:
+            if time.monotonic() > self._entered_band + self.settle_time.value:
+                if self.state.value != 'stable':
+                    await self.state.write('stable')
+        else:
+            if self.state.value != 'settling':
+                await self.state.write('settling')
+
         await instance.write(value=T)
 
     @setpoint.putter
     async def setpoint(self, instance, value):
         self._T0 = time.monotonic()
+        # ensure it flashes low
+        await self.state.write('stable')
+        await self.state.write('settling')
         return value
 
 
